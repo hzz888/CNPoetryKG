75d8204e098bce7d658b0a81066339d41ba31504
hzz <1015048862@qq.com>
2022-05-07 23:36:24 +0800

    default

--- bert_bilstm/BertBiLSTM.py
+++ bert_bilstm/BertBiLSTM.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-np.random.seed(0)
+np.random.seed(0) # set same seed for all processes
 import pandas as pd
 
 import os
@@ -20,7 +20,7 @@ import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 from _thread import start_new_thread
-from functools import wraps
+from functools import wraps # wraps装饰器
 import traceback
 
 import pandas as pd
@@ -39,20 +39,20 @@ class SentimentClassifier(nn.Module):
 
     def __init__(self, hidden_dim, n_layers, n_classes):
         super().__init__()
-        # BERT layers
+        # BERT 层
         self.bert = BertModel.from_pretrained(pretrained_model_name_or_path=".")
-        # LSTM layers
+        # LSTM 层
         self.lstm = nn.LSTM(
-            input_size=self.bert.config.hidden_size,
-            hidden_size=hidden_dim,
+            input_size=self.bert.config.hidden_size, # 将bilstm的输入尺寸设置为bert的词嵌入长度
+            hidden_size=hidden_dim, # bilstm词嵌入长度设为自身隐藏状态大小
             num_layers=n_layers,
             batch_first=True,
-            bidirectional=True
+            bidirectional=True # bilstm是双向的
         )
 
-        self.drop = nn.Dropout(p=0.5)
+        self.drop = nn.Dropout(p=0.5) # 防止过拟合
         self.out = nn.Sequential(
-            nn.Linear(hidden_dim * 2 * n_layers, hidden_dim),  # Linear layer
+            nn.Linear(hidden_dim * 2 * n_layers, hidden_dim),  # 线性层
             nn.Linear(hidden_dim, hidden_dim),
             nn.ELU(),
             nn.Linear(hidden_dim, n_classes)
@@ -63,7 +63,8 @@ class SentimentClassifier(nn.Module):
         # print(output.last_hidden_state.shape)
         embedded = output.last_hidden_state
 
-        self.lstm.flatten_parameters()  # for warning below
+        self.lstm.flatten_parameters()  # 重置参数数据指针
+        # for warning below
         # UserWarning: RNN module weights are not part of single contiguous chunk of memory. 
         # This means they need to be compacted at every call, possibly greatly increasing memory usage. 
         # To compact weights again call flatten_parameters(). 
@@ -132,7 +133,7 @@ def setup(rank, world_size):
     dist_init_method = 'tcp://{master_ip}:{master_port}'.format(
         master_ip='127.0.0.1', master_port='12345')
 
-    # initialize the process group
+    # initialize the process group to do distributed training
     dist.init_process_group(backend="nccl", init_method=dist_init_method,
                             rank=rank, world_size=world_size)
 
@@ -149,20 +150,20 @@ def run_(rank, world_size, data):
     model = SentimentClassifier(hidden_dim=256, n_layers=2, n_classes=4).to(rank)
     if rank == 0:
         print(model)
-        total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        total_params = sum(p.numel() for p in model.parameters() if p.requires_grad) # 查看模型参数数量
         print(f"model total params {total_params}")
     # ddp model
     ddp_model = DDP(model, device_ids=[rank], output_device=rank, find_unused_parameters=True)
 
     loss_fn = nn.CrossEntropyLoss()
 
-    EPOCHS = 10
+    EPOCHS = 10 # 训练轮数
     tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path=".")  # bert-base-chinese
     optimizer = AdamW(ddp_model.parameters(), lr=5e-5, correct_bias=False, no_deprecation_warning=True)
 
     # Split train_nid and create dataloader
     train_dataset = TMPDataset(data['data_train'])
-
+    # shuffle data
     sampler = DistributedSampler(train_dataset)
     train_dataloader = DataLoader(
         train_dataset, shuffle=False, drop_last=True,
@@ -203,7 +204,7 @@ def run_(rank, world_size, data):
         torch.distributed.barrier()
         time_end = time.time()
 
-        # validation after each epoch
+        # validation master node(rank==0) after each epoch
         if rank == 0:
             print(f'Epoch {epoch:2} Train Time(s): {time_end - time_start:4.2f}', end=' ')
             ddp_model.eval()
@@ -254,8 +255,7 @@ def main_():
     data = dict()
     data['data_train'] = data_train
     data['data_test'] = data_test
-    data['bs'] = 32
-
+    data['bs'] = 32 # set batch size to 32
     data['n_epoch'] = 10
 
     world_size = 8  # distributed training
